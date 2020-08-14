09e1513a9929d305815b981931bcf4dc7900639d
KoHey94@X1C <sugawara622@gmail.com>
2020-08-14 23:26:12 +0900

    _

--- NanoFrameworkDemo/Program.cs
+++ NanoFrameworkDemo/Program.cs
@@ -27,30 +27,53 @@ namespace NanoFrameworkDemo
                 Thread.Sleep(500);
             }
             */
-            
+
+            var pin22 = gpio.OpenPin(22);
+            var pin23 = gpio.OpenPin(23);
             var pin25 = gpio.OpenPin(25);
             var pin26 = gpio.OpenPin(26);
+
+            pin22.SetDriveMode(GpioPinDriveMode.Output);
+            pin23.SetDriveMode(GpioPinDriveMode.Output);
             pin25.SetDriveMode(GpioPinDriveMode.Output);
             pin26.SetDriveMode(GpioPinDriveMode.Output);
-            
-            pin25.Write(GpioPinValue.Low);
-            pin26.Write(GpioPinValue.Low);
+
+
             while (true)
             {
+                pin22.Write(GpioPinValue.High);
+                pin23.Write(GpioPinValue.Low);
                 pin25.Write(GpioPinValue.High);
                 pin26.Write(GpioPinValue.Low);
                 Thread.Sleep(500);
+                pin22.Write(GpioPinValue.Low);
+                pin23.Write(GpioPinValue.Low);
                 pin25.Write(GpioPinValue.Low);
                 pin26.Write(GpioPinValue.Low);
                 Thread.Sleep(500);
+                pin22.Write(GpioPinValue.Low);
+                pin23.Write(GpioPinValue.High);
                 pin25.Write(GpioPinValue.Low);
                 pin26.Write(GpioPinValue.High);
                 Thread.Sleep(500);
+                pin22.Write(GpioPinValue.Low);
+                pin23.Write(GpioPinValue.Low);
                 pin25.Write(GpioPinValue.Low);
                 pin26.Write(GpioPinValue.Low);
                 Thread.Sleep(500);
 
             }
+
+        }
+
+        static void SetPin(GpioPin pin)
+        {
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            pin.Write(GpioPinValue.Low);
+        }
+
+        static void Forward()
+        {
 
         }
     }
